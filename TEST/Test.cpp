3018175f34d26730a091c26b996e6c812ab4f51e
danielebellini <daniele.bellini@stud.unifi.it>
2020-09-30 11:33:54 +0200

    last work update

--- TEST/Test.cpp
+++ TEST/Test.cpp
@@ -6,39 +6,40 @@
 
 
 TEST(DimensioneMappa,Mappatest0 ) {
-Mappa map0 = Mappa::crea(0);
-ASSERT_EQ(map0.getRows(),28) << "Map reading problem: height not calculated correctly.";
-ASSERT_EQ(map0.getColumns(),28) << "Map reading problem: width not calculated correctly.";
-std::cout<<"Dimensioni mappa calcolate correttamente"<<endl;
+    Mappa map0 = Mappa::crea(0);
+    ASSERT_EQ(map0.getRows(),28) << "Map reading problem: height not calculated correctly.";
+    ASSERT_EQ(map0.getColumns(),28) << "Map reading problem: width not calculated correctly.";
+    std::cout<<"Dimensioni mappa calcolate correttamente"<<endl;
 }
 
 
 TEST(DimensioneMappa, Mappatest1){
-Mappa map1 = Mappa::crea(0);
-ASSERT_EQ(map1.getRows(),16) << "Small map reading problem: height not calculated correctly.";
-ASSERT_EQ(map1.getColumns(),16) << "Small map reading problem: width not calculated correctly.";
-std::cout<<"Dimensioni mappa calcolate correttamente"<<endl;
+    Mappa map1 = Mappa::crea(0);
+    ASSERT_EQ(map1.getRows(),16) << "Small map reading problem: height not calculated correctly.";
+    ASSERT_EQ(map1.getColumns(),16) << "Small map reading problem: width not calculated correctly.";
+    std::cout<<"Dimensioni mappa calcolate correttamente"<<endl;
 }
 
 TEST(PosizionePersonaggio,check) {
-Mappa map2 = Mappa::crea(0);
-Personaggio pers = Personaggio::crea();
-int x = (rand()% (map2.getColumns()/2)+5);
-int y = (rand() % (map2.getRows()/2)+5);
-Obiettivo obiettivo = Obiettivo(x, y);
-int X = pers.getX();
-int Y = pers.getY();
-map2.tiles[X-1][Y-1] = 204;
-map2.tiles[X-1][Y] = 204;
-map2.tiles[X-1][Y+1] = 204;
-map2.tiles[X][Y-1] = 204;
-map2.tiles[X][Y+1] = 204;
-map2.tiles[X+1][Y-1] = 204;
-map2.tiles[X+1][Y] = 204;
-map2.tiles[X+1][Y+1] = 204;
-Schermo schermo;
-string g = schermo.Astar(obiettivo);
-ASSERT_EQ(g, "Search terminated. Did not find goal state") << "Posizione finale raggiunta correttamente";
-std::cout<<"\nObiettivo non raggiunto"<<endl;
+    Schermo schermo;
+    Mappa map2 = Mappa::crea(0);
+    Personaggio pers = Personaggio::crea();
+    int x = (rand()% (map2.getColumns()/2)+5);
+    int y = (rand() % (map2.getRows()/2)+5);
+    Obiettivo obiettivo = Obiettivo(x, y);
+    int X = pers.getX();
+    int Y = pers.getY();
+    map2.tiles[X-1][Y-1] = 204;
+    map2.tiles[X-1][Y] = 204;
+    map2.tiles[X-1][Y+1] = 204;
+    map2.tiles[X][Y-1] = 204;
+    map2.tiles[X][Y+1] = 204;
+    map2.tiles[X+1][Y-1] = 204;
+    map2.tiles[X+1][Y] = 204;
+    map2.tiles[X+1][Y+1] = 204;
+    map2.load();
+    string g = schermo.Astar(obiettivo);
+    ASSERT_EQ(g, "Search terminated. Did not find goal state") << "Posizione finale raggiunta correttamente";
+    std::cout<<"\nObiettivo non raggiunto"<<endl;
 }
 
